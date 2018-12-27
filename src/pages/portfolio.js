b4a6753409468b518abcbb87ab31c564b6e58013
Zach Hardesty <yhello@zachhardesty.com>
2018-12-27 14:36:59 -0600

    add portfolio dimmer on hover & update gatsby for hooks

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import 'semantic-ui-css/semantic.min.css'
 
-// user-defined
-import './index.scss'
-
 import {
+  Dimmer,
   Segment,
   Container,
   Header,
@@ -28,29 +26,59 @@ import speedway from '../../static/speedway.jpg'
 import springs from '../../static/springs.jpg'
 import vincent from '../../static/vincent.jpg'
 
-const images = [baylor, esab, fiveGuys, fortiline, goodyear, lowes, newHope, scottWhite, speedway, springs, vincent]
+const images = [
+  baylor, esab, fiveGuys,
+  fortiline, goodyear, lowes,
+  newHope, scottWhite, speedway,
+  springs, vincent
+]
 
-const portfolio = ({ data }) => (
-  <Template>
-    <Segment padded vertical basic>
-      <Container text>
-        <Header as='h1'>Our Work</Header>
-        {/* <Header.Content>description</Header.Content> */}
-      </Container>
-    </Segment>
-    <Segment padded vertical basic>
-      <Container>
-        <Grid textAlign='center' columns={3} stackable>
-          {images.map(image => (
-            <Grid.Column>
-              <Image centered className='portfolio-image' src={image} />
-            </Grid.Column>
-          ))}
-        </Grid>
-      </Container>
-    </Segment>
-  </Template>
-)
+const portfolio = ({ data }) => {
+  const [hovered, setHovered] = useState('')
+
+  return (
+    <Template>
+      <Segment padded vertical basic>
+        <Container text>
+          <Header as='h1'>Our Work</Header>
+          {/* <Header.Content>description</Header.Content> */}
+        </Container>
+      </Segment>
+      <Segment padded vertical basic>
+        <Container>
+          <Grid
+            textAlign='center'
+            columns={3}
+            stackable
+          >
+            {images.map(image => (
+              <Grid.Column>
+                <Dimmer.Dimmable
+                  className='portfolio-item'
+                  // REVIEW: use id not url
+                  dimmed={image === hovered}
+                  onMouseEnter={() => setHovered(image)}
+                  onMouseLeave={() => setHovered('')}
+                >
+                  <Dimmer inverted simple>
+                    <Header as='h2'>
+                      Speedway Commerce Center
+                    </Header>
+                    <Header as='h3'>
+                      Charlotte Motor Speedway, Concord, NC
+                    </Header>
+                  </Dimmer>
+
+                  <Image centered className='portfolio-image' src={image} />
+                </Dimmer.Dimmable>
+              </Grid.Column>
+            ))}
+          </Grid>
+        </Container>
+      </Segment>
+    </Template>
+  )
+}
 
 portfolio.propTypes = {
   data: PropTypes.object // eslint-disable-line react/forbid-prop-types
