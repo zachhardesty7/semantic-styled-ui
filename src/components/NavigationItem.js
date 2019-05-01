71fe816eeb0e80c786e14afede5f72d55c6b2ee0
Zach Hardesty <zachhardesty7@gmail.com>
2019-04-30 23:42:57 -0500

    hotfix odd namespace parsing bug

--- src/components/NavigationItem.js
+++ src/components/NavigationItem.js
@@ -16,12 +16,6 @@ const S = {} // styled-components namespace
 
 const FilteredItem = asTag(withoutProps(Menu.Item, ['pointing', 'activeClassName']))
 S.Item = styled(FilteredItem)`
-  @media ${media.phone} {
-    a {
-      font-size: 0.97rem;
-    }
-  }
-
   ${({ pointing }) => pointing && css`
     border-bottom: 2px solid rgba(34,36,38,.15);
 
@@ -39,6 +33,13 @@ S.Item = styled(FilteredItem)`
       background-color: rgba(0,0,0,.05);
     }
   `};
+
+  /* REVIEW: report bug when placing this block before the prev */
+  @media ${media.phone} {
+    a {
+      font-size: 0.97rem;
+    }
+  }
 `
 
 const NavigationItem = ({
