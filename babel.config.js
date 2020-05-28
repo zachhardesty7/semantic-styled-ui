3695f4d0851ef4d96d8657c7e4cf4f7bc486fafc
Zach Hardesty <hello@zachhardesty.com>
2020-05-28 16:43:53 -0500

    update pkg build method

--- babel.config.js
+++ babel.config.js
@@ -4,14 +4,25 @@ module.exports = {
       '@babel/preset-env', {
         targets: '> 0.25%, not dead',
         loose: true,
-        // useBuiltIns: 'usage', // TODO: currently breaks things in Gatsby projects
-        shippedProposals: true,
+        bugfixes: true,
+        // don't polyfill while commented out
+        // useBuiltIns: 'usage', // TODO: currently breaks things in Gatsby projects,
+        // https://babeljs.io/docs/en/babel-preset-env#forcealltransforms
+        shippedProposals: true, // https://babeljs.io/docs/en/babel-preset-env#shippedproposals
+
+        // "amd" | "umd" | "systemjs" | "commonjs" | "cjs" | "auto" | false, defaults to "auto".
+        // Enable transformation of ES6 module syntax to another module type.
+        // Setting this to false will not transform modules.
         modules: false,
       },
     ],
     [
       '@babel/preset-react', {
-        pragma: 'React.createElement',
+        // https://babeljs.io/docs/en/babel-preset-react/#react-automatic-runtime
+        runtime: 'automatic',
+        // useBuiltIns: true,
+        // useSpread: true,
+        development: process.env.ENV_MODE === 'local',
       },
     ],
   ],
@@ -21,12 +32,15 @@ module.exports = {
         cssNamespace: '&&&&&&',
       },
     ],
-    [
-      'styled-components', {
-        pure: true,
-        fileName: false,
-        displayName: false,
-      },
-    ],
+    // [
+    //   'styled-components', {
+    //     // pure: true,
+    //     // fileName: false,
+    //     // displayName: false,
+    //     pure: false,
+    //     fileName: true,
+    //     displayName: true,
+    //   },
+    // ],
   ],
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,90 @@
 import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+// https://github.com/rollup/rollup/issues/1089#issuecomment-402109607
+import path from 'path'
+
+const onwarn = (warning, rollupWarn) => {
+  const ignoredWarnings = [
+    {
+      ignoredCode: 'CIRCULAR_DEPENDENCY',
+      ignoredPath: 'node_modules/semantic-ui-react/dist/',
+    },
+  ]
+
+  // only show warning when code and path don't match
+  // anything in above list of ignored warnings
+  if (!ignoredWarnings.some(({ ignoredCode, ignoredPath }) => (
+    warning.code === ignoredCode &&
+    warning.importer.includes(path.normalize(ignoredPath))))
+  ) {
+    rollupWarn(warning)
+  }
+}
+
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'react/jsx-dev-runtime': 'jsxDevRuntime',
+  'styled-components': 'styled',
+}
+
+const prodBundles = [
+  {
+    file: 'dist/bundle.umd.js',
+    format: 'umd',
+    name: 'SSUI',
+    globals,
+  },
+  {
+    file: 'dist/bundle.min.umd.js',
+    format: 'umd',
+    name: 'SSUI',
+    globals,
+    plugins: [terser()],
+  },
+  {
+    // dir: 'dist',
+    file: 'dist/bundle.cjs.js',
+    format: 'cjs',
+  },
+  {
+    // dir: 'dist',
+    file: 'dist/bundle.min.cjs.js',
+    format: 'cjs',
+    plugins: [terser()],
+  },
+  {
+    // dir: 'dist',
+    file: 'dist/bundle.esm.js',
+    format: 'esm',
+  },
+  {
+    // dir: 'dist',
+    file: 'dist/bundle.min.esm.js',
+    format: 'esm',
+    plugins: [terser()],
+  },
+]
 
 const config = {
   input: 'src/index.js',
-  output: {
-    dir: 'dist',
-    format: 'esm',
+  // preserveModules: true, // REVIEW: cons: slow slow, pros: ??
+  output: process.env.ENV_MODE === 'prod' ? prodBundles : {
+    file: 'dist/bundle.umd.js',
+    format: 'umd',
+    name: 'SSUI',
+    globals,
   },
-  external: ['styled-components', 'react', 'react-dom'],
-  plugins: [babel({ babelHelpers: 'bundled' }), resolve({ extensions: ['.js', '.jsx'] }), commonjs()],
+  external: ['styled-components', 'react', 'react-dom', 'react/jsx-dev-runtime'],
+  plugins: [
+    babel({ babelHelpers: 'bundled' }),
+    resolve({ extensions: ['.js', '.jsx'] }),
+    commonjs(),
+  ],
+  onwarn,
 }
 
 export default config
