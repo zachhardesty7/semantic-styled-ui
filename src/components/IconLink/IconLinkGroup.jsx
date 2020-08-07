8f790734d147f100f41e752ddc6db3e09e2cd5cd
Zach Hardesty <hello@zachhardesty.com>
2020-08-07 00:31:33 -0500

    fix: fix missed file

--- src/components/IconLink/IconLinkGroup.jsx
+++ src/components/IconLink/IconLinkGroup.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 
-import { flexAlignMap, paddingMap, withNewProps } from "../../utils"
+import { flexAlignMap, spacingMap, withNewProps } from "../../utils"
 
 const S = {} // styled-components namespace
 
@@ -10,15 +10,15 @@ S.Groups = styled.div`
   justify-content: ${({ $justify }) => flexAlignMap[$justify]};
 
   ${({ $padded, $padding }) =>
-    ($padded === "top" && `padding-top: ${paddingMap[$padding]}`) ||
-    ($padded === "bottom" && `padding-bottom: ${paddingMap[$padding]}`) ||
-    ($padded && `padding: ${paddingMap[$padding]} 0`)};
+    ($padded === "top" && `padding-top: ${spacingMap[$padding]}`) ||
+    ($padded === "bottom" && `padding-bottom: ${spacingMap[$padding]}`) ||
+    ($padded && `padding: ${spacingMap[$padding]} 0`)};
 `
 
 S.Group = styled.div`
   display: flex;
   margin: 0;
-  padding: 0 ${paddingMap.compact};
+  padding: 0 ${spacingMap.compact};
 
   &:first-child {
     padding-left: 0;
